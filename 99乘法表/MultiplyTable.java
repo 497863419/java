3f7e7b6d4cd350c30ad145ec151caf79533dc2fc
497863419 <497863419@qq.com>
2019-03-07 15:45:10 +0800

    打印9*9乘法表

---
+++ 99乘法表/MultiplyTable.java
@@ -0,0 +1,11 @@
+public class MultiplyTable{
+public static void main(String[] args){
+for(int i=1;i<=9;i++){
+for(int j=1;j<=i;j++){
+System.out.print(j+"*"+i+"="+j*i+"\t");//不要换行，
+}
+System.out.println();//换行
+}
+
+}
+}
