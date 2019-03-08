3b9ad933b7e489d3419a5ac9bc5943cdca88d5c6
497863419 <497863419@qq.com>
2019-03-08 16:58:48 +0800

    private实现封装 对比封装了属性和未封装属性的程序 扩展：getter方法和setter方法 使用

---
+++ private实现封装/Test3.java
@@ -0,0 +1,16 @@
+﻿//无封装程序
+class Person{
+	 String name;
+	 int age;
+	 public void getPersonInfo(){
+		 System.out.println("姓名："+name+",年龄："+age);
+	 }
+}	
+public class Test3{
+	public static void main(String[] args){
+		Person person=new Person();
+		person.name="张三";
+		person.age=18;
+		person.getPersonInfo();
+	}
+}
---
+++ private实现封装/Test4.java
@@ -0,0 +1,32 @@
+﻿//使用private封装属性
+class Person{
+	 private String name;//使用了private对属性进行了封装，要访问私有属性必须使用getter和setter方法
+	 private int age;
+	 public void setName(String n){//setter方法：主要用于进行属性内容的设置与修改
+		name = n;
+	 }
+	 public String getName(){//getter方法：主要用于进行属性内容的取得
+		 return name;
+	 }
+	 public void setAge(int i){
+		 if(i>0&&i<=200){
+			 age = i;
+		 }else{
+			 age = 0;
+		 }
+	 }
+	 public int getAge(){
+		 return age ;
+	 }
+	public void getPersonInfo(){
+		System.out.println("姓名："+name+"年龄："+age);
+	}
+}	
+public  class Test4{
+	public static void main(String[] args){
+		Person person=new Person();
+		person.setName("张三");
+		person.setAge(17);
+		person.getPersonInfo();
+	}
+}
