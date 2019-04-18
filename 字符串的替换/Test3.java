3ddcb324df873382bba9d95645f651a99df982a0
497863419 <497863419@qq.com>
2019-04-18 11:44:09 +0800

    字符串替换方法

---
+++ 字符串的替换/Test3.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class Test3 {
+    public static void main(String[] args) {
+        String str="HelloWorld";
+        System.out.println(str.replaceAll("l","*"));
+        System.out.println(str.replaceFirst("e","*"));
+    }
+}
