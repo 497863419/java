6d938da2c66695add562fe6a2d07d7b33a8c57db
497863419 <497863419@qq.com>
2019-04-20 14:09:18 +0800

    字符串与基本类型转换

---
+++ 基本类型与字符串转换/Test11.java
@@ -0,0 +1,17 @@
+package Test;
+
+import static java.lang.String.valueOf;
+
+public class Test11 {
+    public static void main(String[] args){
+        String str ="1234";
+        int num= Integer.parseInt(str);
+        System.out.println(num+20);
+        System.out.println(valueOf(1));
+        int num1=8;
+        String string = num1+"";
+        System.out.println(string);
+        String str1 = valueOf(100);
+        System.out.println(str1);
+    }
+}
