475a1c8e2cf0fba0bca2c61f5b186c7a656956a0
497863419 <497863419@qq.com>
2019-08-15 15:15:57 +0800

    shellSort

---
+++ shellSort/shellSort.java
@@ -0,0 +1,56 @@
+package com.DS.sort.shellSort;
+
+import java.util.Arrays;
+
+/**
+ * @Name： 希尔排序
+ * @Author：ZYJ
+ * @Date：2019-08-15-14:38
+ * @Description:
+ */
+public class shellSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        System.out.println(Arrays.toString(arr));
+      //  shellSort(arr);
+       // System.out.println("=====================");
+        shellSort2(arr);
+    }
+
+    public static void shellSort(int[] arr) {
+        int temp =0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]){
+                        temp =arr[j];
+                        arr[j] = arr[j+gap];
+                        arr[j+gap] = temp;
+                    }
+
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void shellSort2(int[] arr){
+        for(int gap = arr.length/2;gap>0;gap/=2){
+            for(int i  = gap ; i<arr.length;i++){
+                int j =i ;
+                int temp =arr[j];
+                if(arr[j] <arr[j-gap]){
+                    while (j-gap>=0 && temp<arr[j-gap]) {
+                        arr[j] = arr[j - gap];
+                        j = j - gap;
+                    }
+                }
+                arr[j] =temp;
+
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+
+}
